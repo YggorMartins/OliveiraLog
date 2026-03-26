02b8ef7ddfd40a66c21a9860e14cd5aa030b9cbf
Yggor Martins <yggormartins@gmail.com>
2026-03-26 19:33:35 -0300

    Initial Commit

---
+++ src/controllers/delivery-logs-controller.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+
+class DeliveryLogsController {
+  async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(req.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    if (!delivery) {
+      throw new AppError("delivery not found", 404);
+    }
+
+    if (delivery.status === "delivered") {
+      throw new AppError("this order has already been delivered");
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError("change status to shipped");
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return res.status(201).json();
+  }
+
+  async show(req: Request, res: Response) {
+    const paramsSchema = z.object({
+      delivery_id: z.string().uuid(),
+    });
+
+    const { delivery_id } = paramsSchema.parse(req.params);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+      include: {
+        logs: true,
+        user: true,
+      },
+    });
+
+    if (req.user?.role === "customer" && req.user.id !== delivery?.userId) {
+      throw new AppError("the user can only view their deliveries", 401);
+    }
+
+    return res.json(delivery);
+  }
+}
+
+export { DeliveryLogsController };
---
+++ src/database/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg"
+
+
+export const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ 
+  adapter,
+  log: process.env.NODE_ENV === "production" ? [] : ["query"]
+});
